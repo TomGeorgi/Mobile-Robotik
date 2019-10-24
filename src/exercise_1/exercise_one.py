7d8a4b4e2976f97f416e612b2bde5bd716fc545a
da721wol <da721wol@gmx.de>
2019-10-24 14:37:20 +0200

    AUfgabe 2 gerechnet - Punkt-Matrix-Multiplikation fehlt

--- src/exercise_1/exercise_one.py
+++ src/exercise_1/exercise_one.py
@@ -62,7 +62,7 @@ def rot_x(theta):
         np.array of a 3D rotation matrix around the rotation axis x.
     """
     theta = np.radians(theta)
-    return np.array([[1, 0, 0], [0, np.cos(theta), - np.sin(theta)], [0, np.sin(theta), np.cos(theta)]])
+    return np.array([[1, 0, 0], [0, np.round(np.cos(theta), 2), - np.round(np.sin(theta), 2)], [0, np.round(np.sin(theta), 2), np.round(np.cos(theta), 2)]])
 
 
 def rot_y(theta):
@@ -77,7 +77,7 @@ def rot_y(theta):
         np.array of a 3D rotation matrix around the rotation axis y.
     """
     theta = np.radians(theta)
-    return np.array([[np.cos(theta), 0, np.sin(theta)], [0, 1, 0], [- np.sin(theta), 0, np.cos(theta)]])
+    return np.array([[np.round(np.cos(theta), 2), 0, np.round(np.sin(theta), 2)], [0, 1, 0], [np.round(- np.sin(theta), 2), 0, np.round(np.cos(theta), 2)]])
 
 
 def rot_z(theta):
@@ -92,7 +92,7 @@ def rot_z(theta):
         np.array of a 3D rotation matrix around the rotation axis z.
     """
     theta = np.radians(theta)
-    return np.array([[np.cos(theta), - np.sin(theta), 0], [np.sin(theta), np.cos(theta), 0], [0, 0, 1]])
+    return np.array([[np.round(np.cos(theta), 2), - np.round(np.sin(theta), 2), 0], [np.round(np.sin(theta), 2), np.round(np.cos(theta), 2), 0], [0, 0, 1]])
 
 
 def rot_to_translate(r):
@@ -121,21 +121,83 @@ def rot_to_translate(r):
 
     return arr
 
+def exercise_2_1():
+    #a)
+    tv_a_b = (-2, 0, 0)
+    tv_a_b = trans(tv_a_b)
+    r_a_b = rot_z(180)
+    r_a_b = rot_to_translate(r_a_b)
+    t_a_b = np.dot(tv_a_b, r_a_b);
+    print("T_a_b:", t_a_b)
 
-# Exercise 2.1 a)
-# t_a_b = np.array([[-1, 0, 0, -2], [0, -1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-# t_b_c = np.array([[0, 1, 0, -4], [-1, 0, 0, -1], [0, 0, 1, 0], [0, 0, 0, 1]])
-# t_a_c = np.array([[0, -1, 0, 2], [1, 0, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]])
+    tv_b_c = (-4, -1, 0)
+    tv_b_c = trans(tv_b_c)
+    r_b_c = rot_z(-90)
+    r_b_c = rot_to_translate(r_b_c)
+    t_b_c = np.dot(tv_b_c, r_b_c);
+    print("T_b_c:", t_b_c)
 
-# Exercise 2.1 b)
-# print(t_a_c)
-# print(np.matmul(t_a_b, t_b_c))
-# if np.array_equal(t_a_c, np.matmul(t_a_b, t_b_c)):
-#     print("True")
-# else:
-#     print("False")
+    tv_a_c = (2, 1, 0)
+    tv_a_c = trans(tv_a_c)
+    r_a_c = rot_z(90)
+    r_a_c = rot_to_translate(r_a_c)
+    t_a_c = np.dot(tv_a_c, r_a_c);
+    print("T_a_c:", t_a_c)
+
+    #b)
+    t_a_c_2 = np.dot(t_a_b, t_b_c)
+    print(t_a_c == t_a_c_2)
+
+    #c)
+    t_c_a_2 = np.linalg.inv(t_a_c)
+    tv_c_a = (-1, 2, 0)
+    tv_c_a = trans(tv_c_a)
+    r_c_a = rot_z(90)
+    r_c_a = rot_to_translate(r_c_a)
+    t_c_a = np.dot(tv_c_a, r_c_a)
+    print(t_c_a_2 == t_c_a)
+
+    #d)
+    p_c = [-2, 1, 0, 1]
+    print("t_a_b:", t_a_c)
+    p_a = np.dot(p_c, t_a_c)
+    print("p_a:" , p_a)
+
+def exercise_2_2():
+    #a)
+    tv_0_a = (1, 1)
+    tv_0_a = trans(tv_0_a)
+    r_0_a = rot(0)
+    r_0_a = rot_to_translate(r_0_a)
+    t_0_a = np.dot(tv_0_a, r_0_a)
+    print("T_0_a:", t_0_a)
+
+    tv_0_b = (3, 2)
+    tv_0_b = trans(tv_0_b)
+    r_0_b = rot(30)
+    r_0_b = rot_to_translate(r_0_b)
+    t_0_b = np.dot(tv_0_b, r_0_b)
+    print("T_0_b:", t_0_b)
+
+    #b)
+    p_b = [[1], [1]]
+    #....
+
+    #c)
+    t_a_0 = np.linalg.inv(t_0_a)
+    t_a_b = np.dot(t_a_0, t_0_b)
+    print(t_a_b)
+
+    #d)
+    #Gleiches Problem
+    #p_a
+
+    #e)
+    #....
 
 if __name__ == '__main__':
     """ main """
-    # placeholder
-    # print(rot_to_translate(np.identity(2)))
+    #exercise_2_1()
+    exercise_2_2()
+
+
