da16dfd6a45d242ec5251e9440f04c2895b740fd
Tom Georgi <tom.georgi@htwg-konstanz.de>
2019-11-07 17:21:21 +0100

    exercise b

--- src/exercise_1/exercise_two.py
+++ src/exercise_1/exercise_two.py
@@ -10,39 +10,54 @@ author:       Tom Georgi, Florian Djokaj, David Wolpers
 date:         17.10.2019
 
 """
+import math
 import numpy as np
-import exercise_one as ex
+import src.exercise_1.exercise_one as ex
 
+
+x_R = 2
+y_R = 1
+theta = 30
+l = 0.6
+h = 0.2
+r = 0.1
+a = 0.1
+b = 0.1
+alpha = 40
+l_one = 0.5
+beta_one = 30
+l_two = 0.5
+beta_two = -10
 
 def exercise_2_a():
     # t_O_R
-    t_O_R = ex.create_transformationmatrix_z(0, (0, 0, 0.1))
+    t_O_R = ex.create_transformationmatrix_z(theta, (x_R, y_R, r))
 
     # t_R_DB
-    t_R_DB = ex.create_transformationmatrix_z(0, (0.6 / 2 - 0.1 / 2, 0, 0.2))
+    t_R_DB = ex.create_transformationmatrix_z(0, (l / 2 - a / 2, 0, h))
 
     # t_DB_D, Rotation um 2 Achsen
-    tv_DB_D = (0, 0, 0.1 / 2)
+    tv_DB_D = (0, 0, b / 2)
     tv_DB_D = ex.trans(tv_DB_D)
-    r_DB_D_z = ex.rot_z(40)
+    r_DB_D_z = ex.rot_z(alpha)
     r_DB_D_z = ex.rot_to_translate(r_DB_D_z)
     r_DB_D_x = ex.rot_x(90)
     r_DB_D_x = ex.rot_to_translate(r_DB_D_x)
     t_DB_D = np.dot(tv_DB_D, r_DB_D_z, r_DB_D_x)
 
     # t_D_A1, Verschiebung um 2 Vektoren
-    tv_D_A1_1 = (0, 0, 0.1 / 2)
+    tv_D_A1_1 = (0, 0, a / 2)
     tv_D_A1_1 = ex.trans(tv_D_A1_1)
-    r_D_A1_z = ex.rot_z(30)
+    r_D_A1_z = ex.rot_z(beta_one)
     r_D_A1_z = ex.rot_to_translate(r_D_A1_z)
-    tv_D_A1_2 = (0.5, 0, 0)
+    tv_D_A1_2 = (l_one, 0, 0)
     tv_D_A1_2 = ex.trans(tv_D_A1_2)
     t_D_A1 = np.dot(tv_D_A1_1, r_D_A1_z, tv_D_A1_2)
 
     # t_A1_A2, erst matirx dann vektor
-    tv_A1_A2 = (0.5, 0, 0)
+    tv_A1_A2 = (l_two, 0, 0)
     tv_A1_A2 = ex.trans(tv_A1_A2)
-    r_A1_A2_z = ex.rot_z(-10)
+    r_A1_A2_z = ex.rot_z(beta_two)
     r_A1_A2_z = ex.rot_to_translate(r_A1_A2_z)
     t_A1_A2 = np.dot(r_A1_A2_z, tv_A1_A2)
 
@@ -54,10 +69,52 @@ def exercise_2_a():
     print('p_O: \n', p_O)
 
 
-#def exercise_2_b():
+def exercise_2_b():
+    pass
+
+
+def backwards_kinematic(point: tuple):
+    x, y, z = point
+
+    t_DB_R = ex.create_transformationmatrix_z(0, (-l / 2, 0, -h))
+    t_R_DB = ex.create_transformationmatrix_z(0, (l / 2 - a / 2, 0, h))
+
+    alpha_tmp = np.arctan2(y, (x - l / 2))
+    alpha_tmp = np.rad2deg(alpha_tmp)
+
+    tv_DB_D = (0, 0, 0 / 2)
+    tv_DB_D = ex.trans(tv_DB_D)
+    r_DB_D_z = ex.rot_z(alpha_tmp)
+    r_DB_D_z = ex.rot_to_translate(r_DB_D_z)
+    r_DB_D_x = ex.rot_x(90)
+    r_DB_D_x = ex.rot_to_translate(r_DB_D_x)
+    t_DB_D = np.dot(tv_DB_D, r_DB_D_z, r_DB_D_x)
+
+    t_R_D = np.dot(t_R_DB, t_DB_D)
+
+    t_D_R = np.linalg.inv(t_R_D)
+    p_D = np.dot(t_R_D, [x, y, z, 1])
+
+    x, y, z = (p_D[0], p_D[1], p_D[2])
+
+    a_tmp = math.sqrt((math.pow(x, 2) + math.pow(y, 2)))
+    c_tmp = (math.pow(a_tmp, 2) - math.pow(l_one, 2) - math.pow(l_two, 2)) / (2 * l_one)
+    print(a_tmp)
+    print(c_tmp)
+    b_tmp = math.sqrt(np.absolute((math.pow(l_two, 2) - math.pow(c_tmp, 2))))
+    print(b_tmp)
+
+    beta_two_tmp = np.rad2deg(np.arctan2(b_tmp, c_tmp))
+    beta_one_tmp = np.rad2deg(np.arctan2(y, x) - np.arctan2(b_tmp, (l_one + c_tmp)))
+
+    print("Alpha: ", str(alpha_tmp))
+    print("Beta One: ", str(beta_one_tmp))
+    print("Beta Two: ", str(beta_two_tmp))
 
 
 if __name__ == '__main__':
     """ main """
     exercise_2_a()
     # exercise_2_2()
+
+    backwards_kinematic((1, 2, 3))
